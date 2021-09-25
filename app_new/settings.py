ed2e0b077aaaf282a8a6947c5148616007376521
Serebrennikov Stanislav <goodsmileduck@gmail.com>
2021-09-25 20:24:21 +0800

    update

--- app_new/settings.py
+++ app_new/settings.py
@@ -2,7 +2,7 @@
 from envparse import env
 DB_HOST = env('DB_HOST', default='localhost')
 DB_PORT = env('DB_PORT', cast=int, default=27017)
-DB_NAME = env('DB_NAME', default='neuroslav')
+DB_NAME = env('DB_NAME', default='erundopel-stg')
 VERSION = env('VERSION', default='0.0.1')
 CHATBASE_API_KEY = env('CHATBASE_API_KEY', default='6f36ad77-b9d9-415c-bed7-6ee68943d64d')
 CHATBASE_BOT_PLATFORM = env('CHATBASE_BOT_PLATFORM', default='alisa')
